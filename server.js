ef802edb72c8b2dd0c5139ce85deeea77f3fcc0d
Niels <niels@w3b.net>
2017-05-27 12:14:31 +0000

    Add new file

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const
+    Feed = require('rss-to-json'),
+    express = require('express'),
+    app = express(),
+    port = 3000;
+
+app.use('/', function (req, res, next) {
+    console.log('Request URL: ' + req.url);
+    next()
+});
+
+app.get('/json', function (req, res) {
+    if (req.query.rss) {
+        Feed.load(req.query.rss, function (err, rss) {
+            res.send(rss);
+        })
+    } else {
+        res.send('Please use ?rss= format')
+    }
+});
+
+app.listen(port);
