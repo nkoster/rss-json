ca72ad32221c631a6710d9b4870b84bcf63676ac
Niels <niels@w3b.net>
2017-05-28 23:06:35 +0200

    work around exception, eek!

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 'use strict';
 
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
+});
+
 const
     Feed = require('rss-to-json'),
     express = require('express'),
@@ -16,7 +20,11 @@ app.use('/', function (req, res, next) {
 app.get('/json', function (req, res) {
     if (req.query.rss) {
         Feed.load(req.query.rss, function (err, rss) {
-            res.send(rss);
+            if (err) {
+                res.send("error");
+            } else {
+                res.send(rss)
+            }
         })
     } else {
         res.send('Please use ?rss= format')
